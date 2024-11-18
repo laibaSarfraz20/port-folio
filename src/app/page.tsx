c239d53f6bf39c15901c9da9b1ea364bc5e7e4c9
laibaSarfraz20 <154825429+laibaSarfraz20@users.noreply.github.com>
2024-11-18 22:51:29 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image';
 import COVID from './assets/COVID.png'
 import NIKE from './assets/NIKE.png'
 import QUIZ from './assets/QUIZ.png'
-import post from './assets/post.png'
 import portfolio  from './assets/portfolio.png'
+import me from './assets/me.png'
+import calculator from './assets/calculator.png'
+import ebook from './assets/ebook.png'
 
 export default function Home() {
   return (
@@ -41,9 +43,13 @@ export default function Home() {
             About Me
           </h2>
           <div className="flex flex-col md:flex-row items-center">
-            <img alt="A professional headshot of the portfolio owner" className="rounded-full w-52 h-52 md:w-50` md:h-48 mb-4 md:mb-0 md:mr-6" height="150" src="https://storage.googleapis.com/a1aa/image/dA886njQ65pXKRwTcLDKEF1jeXDKytJPXF1BN2tzC6YfelgnA.jpg" width="150" />
+            <Image src={me} alt="A professional headshot of the portfolio owner" className=" w-60 h-90 md:w-60` md:h-50 mb-4 md:mb-0 md:mr-6" height="150"  width="150" />
             <p className="text-gray-700 text-lg">
-              Hello! I'm a passionate developer with experience in building web applications using React and Tailwind CSS. I love creating beautiful and functional user interfaces.
+              Hello!I'm An Aspiring Front-End Developer with a solid foundation in HTML, CSS, JavaScript, React, and Tailwind CSS.
+                Currently contributing to real-world projects at a start-up while continuing to learn and expand my
+                expertise in web development. Proficient in using GitHub for version control and Firebase for back-end
+                integration. Passionate about growing my skills and eager to take on new opportunities to develop
+                further within the industry.
             </p>
           </div>
         </section>
@@ -75,7 +81,7 @@ export default function Home() {
               </div>
             </div>
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <Image src ={post} alt="Screenshot of Project 3" className="w-full h-48 object-cover" height="200"  width="300" />
+              <Image src ={ebook} alt="Screenshot of Project 3" className="w-full h-48 object-cover" height="200"  width="300" />
               <div className="p-4">
                 <h3 className="text-xl font-bold text-gray-800">
                   Project 3
@@ -108,7 +114,7 @@ export default function Home() {
               </div>
             </div>
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <Image src={} alt="Screenshot of Project 6" className="w-full h-48 object-cover" height="200" width="300" />
+              <Image src={calculator} alt="Screenshot of Project 6" className="w-full h-48 object-cover" height="200" width="300" />
               <div className="p-4">
                 <h3 className="text-xl font-bold text-gray-800">
                   Project 6
