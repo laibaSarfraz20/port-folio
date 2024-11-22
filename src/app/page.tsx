2404244ca83a747e5f8815defbdea61479638a39
laibaSarfraz20 <154825429+laibaSarfraz20@users.noreply.github.com>
2024-11-22 20:24:11 +0500

    updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,9 +43,9 @@ export default function Home() {
             About Me
           </h2>
           <div className="flex flex-col md:flex-row items-center">
-            <Image src={me} alt="A professional headshot of the portfolio owner" className=" w-60 h-90 md:w-60` md:h-50 mb-4 md:mb-0 md:mr-6" height="150"  width="150" />
+            <Image src={me} alt="A professional headshot of the portfolio owner" className=" w-60 h-90 md:w-60 md:h-50 mb-4 md:mb-0 md:mr-6" height={150}  width={150} />
             <p className="text-gray-700 text-lg">
-              Hello!I'm An Aspiring Front-End Developer with a solid foundation in HTML, CSS, JavaScript, React, and Tailwind CSS.
+              Hello!I&apos;m An Aspiring Front-End Developer with a solid foundation in HTML, CSS, JavaScript, React, and Tailwind CSS.
                 Currently contributing to real-world projects at a start-up while continuing to learn and expand my
                 expertise in web development. Proficient in using GitHub for version control and Firebase for back-end
                 integration. Passionate about growing my skills and eager to take on new opportunities to develop
@@ -59,9 +59,9 @@ export default function Home() {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <Image src ={COVID} alt="Screenshot of Project 1" className="w-full h-48 object-cover" height="200"  width="300" />
+              <Image src ={COVID} alt="Screenshot of Project 1" className="w-full h-48 object-cover" height={200}  width={300} />
               <div className="p-4">
-                <h3 className="text-xl font-bold text-whit-800">
+                <h3 className="text-xl font-bold text-gray-800">
                   Project 1
                 </h3>
                 <p className="text-gray-600">
@@ -70,7 +70,7 @@ export default function Home() {
               </div>
             </div>
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <Image src ={NIKE} alt="Screenshot of Project 2" className="w-full h-48 object-cover" height="200"  width="300" />
+              <Image src ={NIKE} alt="Screenshot of Project 2" className="w-full h-48 object-cover" height={200}  width={300} />
               <div className="p-4">
                 <h3 className="text-xl font-bold text-gray-800">
                   Project 2
@@ -81,7 +81,7 @@ export default function Home() {
               </div>
             </div>
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <Image src ={ebook} alt="Screenshot of Project 3" className="w-full h-48 object-cover" height="200"  width="300" />
+              <Image src ={ebook} alt="Screenshot of Project 3" className="w-full h-48 object-cover" height={200}  width={300} />
               <div className="p-4">
                 <h3 className="text-xl font-bold text-gray-800">
                   Project 3
@@ -92,7 +92,7 @@ export default function Home() {
               </div>
             </div>
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <Image src ={QUIZ} alt="Screenshot of Project 4" className="w-full h-48 object-cover" height="200"  width="300" />
+              <Image src ={QUIZ} alt="Screenshot of Project 4" className="w-full h-48 object-cover" height={200}  width={300}/>
               <div className="p-4">
                 <h3 className="text-xl font-bold text-gray-800">
                   Project 4
@@ -103,7 +103,7 @@ export default function Home() {
               </div>
             </div>
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <Image src={portfolio} alt="Screenshot of Project 5" className="w-full h-48 object-cover" height="200"  width="300" />
+              <Image src={portfolio} alt="Screenshot of Project 5" className="w-full h-48 object-cover" height={200}  width={300} />
               <div className="p-4">
                 <h3 className="text-xl font-bold text-gray-800">
                   Project 5
@@ -114,7 +114,7 @@ export default function Home() {
               </div>
             </div>
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-              <Image src={calculator} alt="Screenshot of Project 6" className="w-full h-48 object-cover" height="200" width="300" />
+              <Image src={calculator} alt="Screenshot of Project 6" className="w-full h-48 object-cover" height={200} width={300}/>
               <div className="p-4">
                 <h3 className="text-xl font-bold text-gray-800">
                   Project 6
@@ -150,7 +150,7 @@ export default function Home() {
               <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" rows={5}></textarea>
             </div>
             <div className="flex items-center justify-between">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                 Send
               </button>
             </div>
